9a98cdce444b603760769fcf2369b152e10e0675
Stefan Mai <iamnafets@gmail.com>
2014-11-30 15:20:47 -0800

    Error handling

--- app/jobs/Scrape.scala
+++ app/jobs/Scrape.scala
@@ -24,62 +24,70 @@ object Scrape {
   Logger.info("Writing to " + landingDirectory.toString)
 
   def scrapeApi = {
-    Logger.info("Calling Car2Go API")
-    val cityScrapes = Cities.map { city =>
-      (city, WS
-        .url("http://www.car2go.com/api/v2.1/vehicles?loc=" + city + "&oauth_consumer_key=car2gowebsite&format=json")
-        .get)
-    }
-
-    cityScrapes.map {
-      case (city, future) =>
-        future.recover {
-          case e: Exception =>
-            Logger.error("Failed to scrape API", e)
-        }
-    }
-
-    cityScrapes map {
-      case (city, future) => future map { response =>
-        val end = System.currentTimeMillis()
-        val destinationFile = Files.createTempFile(landingDirectory, "writing", "")
-        Files.write(destinationFile, response.body.getBytes())
-        Logger.info("Wrote to " + destinationFile + " for city " + city)
-
-        Files.move(
-          destinationFile,
-          destinationFile.resolveSibling("written_" + city + "_" + InetAddress.getLocalHost().getHostName() + "_" + end))
+    try {
+      Logger.info("Calling Car2Go API")
+      val cityScrapes = Cities.map { city =>
+        (city, WS
+          .url("http://www.car2go.com/api/v2.1/vehicles?loc=" + city + "&oauth_consumer_key=car2gowebsite&format=json")
+          .get)
       }
+
+      cityScrapes.map {
+        case (city, future) =>
+          future.recover {
+            case e: Exception =>
+              Logger.error("Failed to scrape API", e)
+          }
+      }
+
+      cityScrapes map {
+        case (city, future) => future map { response =>
+          val end = System.currentTimeMillis()
+          val destinationFile = Files.createTempFile(landingDirectory, "writing", "")
+          Files.write(destinationFile, response.body.getBytes())
+          Logger.info("Wrote to " + destinationFile + " for city " + city)
+
+          Files.move(
+            destinationFile,
+            destinationFile.resolveSibling("written_" + city + "_" + InetAddress.getLocalHost().getHostName() + "_" + end))
+        }
+      }
+    } catch {
+      case e: Exception => Logger.info("Failed in scraping Car2Go API, will try again next time.", e)
     }
   }
 
   def pushToS3 = {
-    Cities map { city =>
-      s3.bucket("scrape2go") map { bucket =>
-        val activeFiles = landingDirectory.toFile.listFiles.filter(_.getName.startsWith("written_" + city))
-        val contentList = activeFiles.map { file =>
-          val content = Source.fromFile(file).mkString
-          val end = file.getName.replace("written_" + city + "_" + InetAddress.getLocalHost().getHostName() + "_", "").toLong
-          "\"" + end + "\": " + content
-        }
-
-        if (!contentList.isEmpty) {
-          val stagingContent = "{ \"" + city + "\": {" + contentList.mkString(",") + "}}"
-          Logger.info("Trying to upload " + stagingContent.size + " segments to S3")
-          val stagingFile = Files.createTempFile(landingDirectory, "to_s3", "")
-          Files.write(stagingFile, stagingContent.getBytes())
-
-          bucket.put(InetAddress.getLocalHost().getHostName() + "/" + city + "/" + System.currentTimeMillis, stagingFile.toFile)
-          Files.delete(stagingFile)
-          Logger.info("Succeeded in upload " + contentList.size + " segments, " + stagingContent.size + " bytes to S3")
-
-          activeFiles.map { file =>
-            file.delete
+    try {
+      Cities map { city =>
+        s3.bucket("scrape2go") map { bucket =>
+          val activeFiles = landingDirectory.toFile.listFiles.filter(_.getName.startsWith("written_" + city))
+          val contentList = activeFiles.map { file =>
+            val content = Source.fromFile(file).mkString
+            val end = file.getName.replace("written_" + city + "_" + InetAddress.getLocalHost().getHostName() + "_", "").toLong
+            "\"" + end + "\": " + content
           }
-        } else {
-          Logger.info("No segments to upload to S3.")
+
+          if (!contentList.isEmpty) {
+            val stagingContent = "{ \"" + city + "\": {" + contentList.mkString(",") + "}}"
+            Logger.info("Trying to upload " + stagingContent.size + " segments to S3")
+            val stagingFile = Files.createTempFile(landingDirectory, "to_s3", "")
+            Files.write(stagingFile, stagingContent.getBytes())
+
+            bucket.put(InetAddress.getLocalHost().getHostName() + "/" + city + "/" + System.currentTimeMillis, stagingFile.toFile)
+            Files.delete(stagingFile)
+            Logger.info("Succeeded in upload " + contentList.size + " segments, " + stagingContent.size + " bytes to S3")
+
+            activeFiles.map { file =>
+              file.delete
+            }
+          } else {
+            Logger.info("No segments to upload to S3.")
+          }
         }
       }
+    } catch {
+      case e: Exception => Logger.info("Failed in uploading to S3, will try again with next run.", e)
     }
   }
 
--- scripts/cloudFormationSetup.sh
+++ scripts/cloudFormationSetup.sh
@@ -1,8 +1,8 @@
 aws cloudformation create-stack \
-  --stack-name Gradient2Go \
+  --stack-name Scrape2Go \
   --template-url https://s3.amazonaws.com/whisper-code/CodeDeploy-West.json \
-  --parameters ParameterKey=InstanceCount,ParameterValue=1 ParameterKey=InstanceType,ParameterValue=t2.micro \
+  --parameters ParameterKey=InstanceCount,ParameterValue=2 ParameterKey=InstanceType,ParameterValue=t2.micro \
     ParameterKey=KeyPairName,ParameterValue=NafPair-West ParameterKey=OperatingSystem,ParameterValue=Linux \
     ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=TagKey,ParameterValue=AppName \
-    ParameterKey=TagValue,ParameterValue=Gradient2Go \
+    ParameterKey=TagValue,ParameterValue=Scrape2Go \
   --capabilities CAPABILITY_IAM
