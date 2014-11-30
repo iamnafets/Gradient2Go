fc0fc12c57005cc71d4423b20e6a511016f6276f
Stefan Mai <iamnafets@gmail.com>
2014-11-30 01:13:30 -0800

    Capture logs and pid

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -5,7 +5,7 @@ set -e
 (
 cd /home/gradient2go
 ./activator clean compile stage &> var/compile
-target/universal/stage/bin/gradient2go
+target/universal/stage/bin/gradient2go &> var/gradient2go &
 PID=$!
 echo $PID > var/pid
 )
