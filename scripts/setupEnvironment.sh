22c1b9c9584bede94368af9243f0dc66bff192fc
Stefan Mai <iamnafets@gmail.com>
2014-11-30 00:53:54 -0800

    Adding installation for javac

--- scripts/setupEnvironment.sh
+++ scripts/setupEnvironment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yum -y install java-1.8.0-openjdk-headless.x86_64
+yum -y install java-1.8.0-openjdk-devel.x86_64
 useradd gradient2go
 mkdir /home/gradient2go
 chown gradient2go:users /home/gradient2go
