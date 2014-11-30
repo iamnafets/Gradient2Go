5dc4f902137b3cb96cd9ba6ed680795e42756d94
Stefan Mai <iamnafets@gmail.com>
2014-11-30 00:24:18 -0800

    Add logging

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/home/gradient2go/activator start &
+/home/gradient2go/activator start 2> /home/gradient2go/var/error > /home/gradient2go/var/log &
 PID=$!
 echo $PID > /home/gradient2go/var/pid
