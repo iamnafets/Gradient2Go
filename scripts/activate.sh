c540e5dc02916a3a15e26948464c94e23fc1760c
Stefan Mai <iamnafets@gmail.com>
2014-11-30 01:07:35 -0800

    New bash syntax

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -4,7 +4,7 @@ set -e
 
 (
 cd /home/gradient2go
-./activator clean compile stage 2>&1 var/compile > var/compile
+./activator clean compile stage &> var/compile
 target/universal/stage/bin/gradient2go
 PID=$!
 echo $PID > var/pid
